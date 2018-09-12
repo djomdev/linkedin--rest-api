8bf27cfe95eaf7d1d35f753d5ec5fede7e426179
Diego Ortega <diegodeortega@MacBook-Pro-de-Diego.local>
2018-09-12 11:23:20 -0500

    part-2-express-server

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,33 +1,19 @@
 # CHANGELOG
 
-**`[1] Basic Node Server`**
+**`[2] Express Server`**
 
 ## Steps
 
-- [X] Create the directory
+- [X] Refactor code and implement `express.js`
 
-- [X] Initialise `package.json` file
+- [X] Implement an environment variable to take the PORT number
 
-- [X] Create the Server Configuration (`app.js`)
+- [X] Write a `npm script` to run the app
 
-- [X] Import modules needed (`http`, `fs`, `chalk`*)
+- [X] Implement `morgan` to log every request in console
 
-- [X] Use `http.createServer()` to create an instance
+- [X] Configure `nodemon` to watch our `app.js`file
 
-- [X] Write a callback inside `.createServer()` that handle the request.
+- [X] Write the **404** and **500** handlers
 
-```js
-(request, response) => {}
-```
-
-- [X] Use `fs` (`file system`) module to read a HTML file
-
-- [X] Configure the header in the response
-
-- [X] Create a basic HTML file and add some styles
-
-- [X] Make the server listen on PORT (`3000`)
-
-- [X] Save PORT in a variable to maintance a better control
-
-- [X] _optional_ Install `chalk` to print colored messages in server console
+- [X] Implement another route to `/api/`
--- app.js
+++ app.js
@@ -1,40 +1,82 @@
 //Import  the modules
-const http = require('http');
+const express = require('express');
 const fs = require('fs');
 const chalk = require('chalk');
+const logger = require('morgan');
+
+/** [0] Express class into app variable */
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 /* Absolute Path to HTML file */
 const indexFile = `${ __dirname }/index.html`;
 
-
 /**
- * [1] Create and instantiate the Node server.
- * [2] The callback function will act like a Listener, it will execute each time
- *      a request is done.
+ * [X] Middlewares
+ * Runs before each request hit the routes configuration.
+ * 
+ * [X] Logs all requests
  * 
  */
+ app.use(logger('dev'));
 
- const app = http.createServer((request, response) => {
-    /**
-     * [1] Read the file as a string.
-     * [2] Send it in the response.
-     * [3] Set the 'Status Code' and the 'Content-Type'.
-     * [4] Finish the response
-     */
-    const string = fs.readFileSync(indexFile);
+ /**
+  * [X] Routes
+  * 'app.use' it's called every time a request is sent to the server.
+  */
+app.get('/api', (request, response) => {
+    response.send('LinkedIn REST API').status(200);
+});
 
-    response.write(string);
-    response.writeHead(200, {
-        "Content-Type": "text/html"
-    });
- });
+app.get('/', (request, response) => {
+    response.sendFile(indexFile);
+});
+
+/**
+ * [X] 404 Not Found
+ * Catch the error.
+ * 
+ * `app.use` it's called everytime a request has been sent to the server.
+ */
+app.use((request, response, next) => {
+    const ERROR_404 = {
+        error: {
+            message: 'The requested resource is not defined.',
+            status: 404
+        }
+    };
+    next(ERROR_404);
+});
+
+/**
+ * [X] 500 Internal Error Server
+ * Catch the error
+ */
+app.use((error, request, response, next) => {
+    const body = error.error;
+    const STATUS_CODE = body.status || 500;
+    const ERROR_505 = body.message || '500. Internal Server Error';
+
+    const formatedMessage = JSON.stringify(error, null, 2);
+
+    response
+        .status(STATUS_CODE)
+        .json({
+            error: {
+                message: ERROR_505,
+                status: STATUS_CODE
+            }
+        });
+        console.log(chalk.red(formatedMessage));
+});
+
 
  /**
   * Run and listen the server on a specific port.
   */
 
-  app.listen(3000, () => {
-      const formatedMessage = chalk.green('Node server running on PORT: 3000');
-      
+  app.listen(PORT, () => {
+      const formatedMessage = chalk.green(`Express server running on PORT: ${ PORT }`);
+
       console.log(formatedMessage);
-  })
+  });
